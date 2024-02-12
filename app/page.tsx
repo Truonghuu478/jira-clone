f451266f6216d54a270072ead8d36845e3a29821
Truonghuu478 <truonghuu478@gmail.com>
2024-02-12 09:22:18 +0700

    page home create table

--- app/page.tsx
+++ app/page.tsx
@@ -10,29 +10,47 @@ export default function Home() {
       </div>
       {/* search  */}
       <div className="">
-          <input type="string"/>
+        <input type="string" />
       </div>
       {/* list  */}
       <div>
         <table>
           <thead>
-              <tr>
-                <td>STT</td>
-                <td>projectName</td>
-                <td>categoryName</td>
-                <td>alias</td>
-                <td>creator</td>
-                <td>members</td>
-                <td>members</td>
-                <td>description</td>
-                <td></td>
-
-
-                
-
-              </tr>
+            <tr>
+              <td>STT</td>
+              <td>projectName</td>
+              <td>categoryName</td>
+              <td>alias</td>
+              <td>creator</td>
+              <td>members</td>
+              <td>description</td>
+              <td></td>
+            </tr>
           </thead>
           <tbody>
+            <tr>
+              <td>
+                1
+              </td>
+              <td>aaaaaa</td>
+              <td>categoryName</td>
+              <td>123</td>
+              <td>creator</td>
+              <td>members</td>
+              <td>description</td>
+              <td>
+                <div className="flex justify-around items-center">
+                  <button>
+                    Xoá
+                  </button>
+                  <button>
+                    Sửa
+                  </button>
+                </div>
+              </td>
+
+            </tr>
+
 
           </tbody>
         </table>
